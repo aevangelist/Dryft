62acaf0717557557d1c24c3038aa3cc85d1e4213
aevangelist <evangelista.aleli@gmail.com>
2016-05-16 18:42:39 -0400

    Redesigned listview item for places

--- app/src/main/java/com/alelievangelista/dryft/api/PlaceListAdapter.java
+++ app/src/main/java/com/alelievangelista/dryft/api/PlaceListAdapter.java
@@ -57,7 +57,7 @@ public class PlaceListAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_place, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item_place_2, parent, false);
 
         ViewHolder viewHolder = new ViewHolder(view);
         view.setTag(viewHolder);
